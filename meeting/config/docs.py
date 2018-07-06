9484e4dc23bf2f9ccbd0585c4c9f191496d45832
root <root@ssnerpdev.c.stable-splicer-183522.internal>
2018-07-06 07:29:47 +0000

    modified docs.py

--- meeting/config/docs.py
+++ meeting/config/docs.py
@@ -2,10 +2,9 @@
 Configuration for docs
 """
 
-# source_link = "https://github.com/[org_name]/meeting"
-# docs_base_url = "https://[org_name].github.io/meeting"
-# headline = "App that does everything"
-# sub_heading = "Yes, you got that right the first time, everything"
+source_link = "https://github.com/vinobc/SSN_meeting"
+headline = "SSN Meeting"
+sub_heading = "Log Minutes of a Meeting @ SSN Institutions"
 
 def get_context(context):
-	context.brand_html = "Meeting"
+	context.brand_html = "SSN_meeting"
