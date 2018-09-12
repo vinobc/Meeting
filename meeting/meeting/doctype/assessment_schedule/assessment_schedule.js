c42ca77400ff864207b30d4e99dcd778576081ed
root <root@snerp2.c.stable-splicer-183522.internal>
2018-09-12 03:27:45 +0000

    added assessment schedule, lesson plan, and monthly activities

---
+++ meeting/meeting/doctype/assessment_schedule/assessment_schedule.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, R Vinob Chander and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Assessment Schedule', {
+	refresh: function(frm) {
+
+	}
+});
