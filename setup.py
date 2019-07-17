a1d428b59371aef77b75371efc3e6469e0bd97fa
root <root@dev-ssnerp.c.stable-splicer-183522.internal>
2019-07-17 02:58:53 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ from setuptools import setup, find_packages
 import re, ast
 
 #get version from version variable in plaid_integration/init.py
-_version_re = re.compile(r’version\s+=\s+(.*)’)
+_version_re = re.compile(r'version\s+=\s+(.*)')
 
-with open(‘requirements.txt’) as f:
-    install_requires = f.read().strip().split(’\n’)
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
 
-    with open(‘meeting/__init__.py’, ‘rb’) as f:
+    with open('meeting/__init__.py', 'rb') as f:
         version = str(ast.literal_eval(_version_re.search(
-            f.read().decode(‘utf-8’)).group(1)))
+            f.read().decode('utf-8')).group(1)))
 
-        #requirements = parse_requirements(“requirements.txt”, session="")
+        #requirements = parse_requirements("requirements.txt", session="")
 
         setup(
-                name=‘meeting’,
+                name='meeting',
                 version=version,
-                description=‘meeting’,
-                author=‘vinob’,
-                author_email=‘vinobchanderr@ssn.edu.in’,
+                description='meeting',
+                author='vinob',
+                author_email='vinobchanderr@ssn.edu.in',
                 packages=find_packages(),
                 zip_safe=False,
                 include_package_data=True,
