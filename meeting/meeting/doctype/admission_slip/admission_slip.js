7026c68115e2ba60db7b94a5339db5ced147b521
root <root@snerp2.c.stable-splicer-183522.internal>
2019-07-17 07:03:10 +0000

    updated

---
+++ meeting/meeting/doctype/admission_slip/admission_slip.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, R Vinob Chander and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Admission Slip', {
+	refresh: function(frm) {
+
+	}
+});
