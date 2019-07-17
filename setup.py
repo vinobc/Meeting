f233595e9f940bdd479c2e74de952ff2821673ca
root <root@dev-ssnerp.c.stable-splicer-183522.internal>
2019-07-17 02:53:57 +0000

    updated

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+#from pip.req import parse_requirements
 import re, ast
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
+#get version from version variable in plaid_integration/init.py
+_version_re = re.compile(r’version\s+=\s+(.*)’)
 
-# get version from __version__ variable in meeting/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+with open(‘requirements.txt’) as f:
+    install_requires = f.read().strip().split(’\n’)
 
-with open('meeting/__init__.py', 'rb') as f:
-	version = str(ast.literal_eval(_version_re.search(
-		f.read().decode('utf-8')).group(1)))
+    with open(‘meeting/__init__.py’, ‘rb’) as f:
+        version = str(ast.literal_eval(_version_re.search(
+            f.read().decode(‘utf-8’)).group(1)))
 
-setup(
-	name='meeting',
-	version=version,
-	description='Meeting management for SSN Institutions',
-	author='R Vinob Chander',
-	author_email='vinobchanderr@ssn.edu.in',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
-)
+        #requirements = parse_requirements(“requirements.txt”, session="")
+
+        setup(
+                name=‘meeting’,
+                version=version,
+                description=‘meeting’,
+                author=‘vinob’,
+                author_email=‘vinobchanderr@ssn.edu.in’,
+                packages=find_packages(),
+                zip_safe=False,
+                include_package_data=True,
+                install_requires=install_requires
+                #dependency_links=[str(ir._link) for ir in requirements if ir._link]
+                )
