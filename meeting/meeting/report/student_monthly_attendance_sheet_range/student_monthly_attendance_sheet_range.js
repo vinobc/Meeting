75b1d6614818e297f0174d9a21982fc0281b8e2e
root <root@snerp2.c.stable-splicer-183522.internal>
2019-09-12 11:58:25 +0000

    added student others

---
+++ meeting/meeting/report/student_monthly_attendance_sheet_range/student_monthly_attendance_sheet_range.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, R Vinob Chander and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Student Monthly Attendance Sheet-range"] = {
+	"filters": [
+
+	]
+};
